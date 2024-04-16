af6b4dc834ca578dc965e35d69da50b5ad2019f0
Paulo Salvatore <salvatoregames@gmail.com>
2024-04-16 14:39:40 +0000

    Criação do endpoint `/oi`

--- index.js
+++ index.js
@@ -5,4 +5,9 @@ app.get('/', function (req, res) {
   res.send('Hello World')
 })
 
+// Exercício: Criar um endpoint [GET] /oi que exibe: "Olá, mundo!"
+app.get('/oi', function (req, res) {
+  res.send('Olá, mundo!')
+})
+
 app.listen(3000)
