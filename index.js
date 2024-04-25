f4098d439b5c2eff048b67b3f054c682ad6c6a0b
Paulo Salvatore <salvatoregames@gmail.com>
2024-04-25 14:53:56 +0000

    Validação no endpoint Update

--- index.js
+++ index.js
@@ -75,18 +75,26 @@ async function main() {
   })
 
   // Endpoint Update -> [PUT] /item/:id
-  app.put('/item/:id', function (req, res) {
+  app.put('/item/:id', async function (req, res) {
     // Obtemos o ID do parâmetro de rota
     const id = req.params.id
 
     // Obtemos o corpo da requisição para saber qual o novo valor
     const novoItem = req.body
 
+    if (!novoItem.nome) {
+      return res.status(400).send("Corpo da requisição sem o campo 'nome'.")
+    }
+
     // Atualizamos o item na collection
-    collection.updateOne(
+    const updateResult = await collection.updateOne(
       { _id: new ObjectId(id) },
       { $set: novoItem }
     )
+
+    if (updateResult.matchedCount === 0) {
+      return res.status(404).send('Item não encontrado.')
+    }
 
     // Enviamos uma mensagem de sucesso
     res.send('Item atualizado com sucesso: ' + id)
